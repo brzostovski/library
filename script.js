ff60d617ca045e1bc0afd0477c701d365d9b8341
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-08-09 21:14:55 +0200

    Format code

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-function Book(
-  title,
-  author,
-  pages,
-  read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-      let readStatus;
-      read ? (readStatus = 'read') : (readStatus = 'not read yet');
-      return `"${title}" by ${author}, ${pages} pages, ${readStatus}.`;
-    }
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+  this.info = function () {
+    let readStatus;
+    read ? (readStatus = "read") : (readStatus = "not read yet");
+    return `"${title}" by ${author}, ${pages} pages, ${readStatus}.`;
+  };
 }
