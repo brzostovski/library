e5bba87004c3fa259224be43cc0cd57c2119e81e
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-08-09 21:11:26 +0200

    Add 'Book' object constructor

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-document.addEventListener('click', () => {
-  console.log('click');
-})
+function Book(
+  title,
+  author,
+  pages,
+  read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+      let readStatus;
+      read ? (readStatus = 'read') : (readStatus = 'not read yet');
+      return `"${title}" by ${author}, ${pages} pages, ${readStatus}.`;
+    }
+}
