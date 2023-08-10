cb7bbd544dfcb0dce0f9f33d166d77b9b376b469
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-08-10 20:22:53 +0200

    Add confirm dialog on cancel button click

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const BOOKS_TABLE = document.querySelector('.books-table-body');
+const NEW_BOOK_FORM = document.querySelector('form');
+const ADD_BOOK_BUTTON = document.querySelector('#add-book-button');
+const CANCEL_BUTTON = document.querySelector('#cancel');
 
 let myLibrary = [
   theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, true),
@@ -29,4 +32,22 @@ function displayBooks(library = myLibrary, table = BOOKS_TABLE) {
   });
 }
 
+function showForm() {
+  NEW_BOOK_FORM.classList.remove('hidden');
+  ADD_BOOK_BUTTON.classList.add('hidden');
+}
+
+function cancelForm() {
+  if (confirm('Are you sure?')) {
+    NEW_BOOK_FORM.classList.add('hidden');
+    ADD_BOOK_BUTTON.classList.remove('hidden');
+  } else {
+    return 0;
+  }
+}
+
 window.addEventListener('load', () => displayBooks());
+
+ADD_BOOK_BUTTON.addEventListener('click', () => showForm());
+
+CANCEL_BUTTON.addEventListener('click', () => cancelForm());
