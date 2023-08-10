a8bae8e2779d2359617f73f3ab2f9b9a6a0d28aa
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-08-10 19:08:35 +0200

    Add function to display books in table

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-let myLibrary = [];
+const BOOKS_TABLE = document.querySelector('.books-table-body');
+
+let myLibrary = [
+  theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, true),
+  harryPotter = new Book('Harry Potter i Zakon Feniksa', 'J.K. Rowling', 600, true),
+  dziennikiGwiazdowe = new Book('Dzienniki Gwiazdowe', 'S. Lem', 300, true),
+];
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-
-  this.info = function () {
-    let readStatus;
-    read ? (readStatus = "read") : (readStatus = "not read yet");
-    return `"${title}" by ${author}, ${pages} pages, ${readStatus}.`;
-  };
 }
 
 function addBookToLibrary(book, library = myLibrary) {
   library.push(book);
 }
+
+function displayBooks(library = myLibrary, table = BOOKS_TABLE) {
+  library.forEach(book => {
+    let newRow = '<tr>';
+    for (key in book) {
+      (key === 'title')
+        ? (newRow += `<td>"${book[key]}"</td>`)
+        : (newRow += `<td>${book[key]}</td>`);
+    }
+    table.innerHTML += `${newRow}</tr>`;
+  });
+}
+
+window.addEventListener('load', () => displayBooks());
